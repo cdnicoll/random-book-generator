f080dce20ce3c2df93627b698de503713e3d99dc
conicoll <conicoll@ea.com>
2017-08-02 09:01:08 -0700

    Added json api routes, started work on view

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,8 +1,17 @@
 package controllers;
 
-import play.mvc.*;
+import javax.inject.Inject;
 
-import views.html.*;
+import com.fasterxml.jackson.databind.JsonNode;
+import play.Logger;
+import play.libs.Json;
+import play.mvc.*;
+import play.libs.ws.*;
+import play.twirl.api.Html;
+import views.html.RandomBookGenerator;
+
+import java.util.Random;
+import java.util.concurrent.CompletionStage;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -10,14 +19,93 @@ import views.html.*;
  */
 public class HomeController extends Controller {
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
-    public Result index() {
-        return ok(index.render("Your new application is ready."));
+    @Inject WSClient ws;
+
+    public CompletionStage<Result> subject(String subject) {
+        String urlQuery = "http://openlibrary.org/subjects/"+subject+".json?published_in=2014-2017&limit=100000&detail=true";
+        Logger.debug("Accessing Url:"+ urlQuery);
+        WSRequest request = ws.url(urlQuery);
+        CompletionStage<WSResponse> completionStage = request.get();
+        //return completionStage.thenApply(response -> ok(response.asJson()));
+        return completionStage.thenApply(response -> {
+            JsonNode json = response.asJson();
+            JsonNode total = json.findPath("work_count");
+            Logger.debug("Work Count: "+ total.asInt());
+            Random rand = new Random();
+            int min = 0;
+            int max = total.asInt();
+            int randomNum = rand.nextInt((max - min) + 0) + min;
+
+            Logger.debug("Random Int: "+randomNum);
+
+
+            System.out.println(randomNum);
+            JsonNode works = json.findPath("works").get(randomNum);
+            //JsonNode works = json.findPath("works");
+            //System.out.println(Json.asciiStringify(json.findPath("works")));
+            return ok(works);
+        });
+    }
+
+    public CompletionStage<Result> bookDetail(String worksUri) {
+        String urlQuery = "https://openlibrary.org/"+worksUri+".json";
+        Logger.debug("Accessing Url:"+ urlQuery);
+        WSRequest request = ws.url(urlQuery);
+        CompletionStage<WSResponse> completionStage = request.get();
+        //return completionStage.thenApply(response -> ok(response.asJson()));
+        return completionStage.thenApply(response -> {
+            JsonNode json = response.asJson();
+            /*
+            JsonNode total = json.findPath("work_count");
+            Logger.debug("Work Count: "+ total.asInt());
+            Random rand = new Random();
+            int min = 0;
+            int max = total.asInt();
+            int randomNum = rand.nextInt((max - min) + 0) + min;
+
+            Logger.debug("Random Int: "+randomNum);
+
+
+            System.out.println(randomNum);
+            JsonNode works = json.findPath("works").get(randomNum);
+            //JsonNode works = json.findPath("works");
+            //System.out.println(Json.asciiStringify(json.findPath("works")));
+            return ok(works);
+            */
+            return ok();
+        });
+    }
+
+
+
+
+
+    public CompletionStage<Result> sample() {
+        String urlQuery = "http://openlibrary.org/subjects/fantasy.json?published_in=2014-2017&limit=100000";
+        Logger.debug("Attempting risky calculation.");
+        WSRequest request = ws.url(urlQuery);
+        CompletionStage<WSResponse> completionStage = request.get();
+        //return completionStage.thenApply(response -> ok(response.asJson()));
+        return completionStage.thenApply(response -> {
+            JsonNode json = response.asJson();
+            JsonNode total = json.findPath("work_count");
+
+            Random rand = new Random();
+            int min = 0;
+            int max = total.asInt();
+            int randomNum = rand.nextInt((max - min) + 1) + min;
+
+            System.out.println(randomNum);
+            JsonNode works = json.findPath("works").get(randomNum);
+            //JsonNode works = json.findPath("works");
+            //System.out.println(Json.asciiStringify(json.findPath("works")));
+            return ok(works);
+        });
+    }
+
+    public Result sampleView()
+    {
+        return ok(RandomBookGenerator.render("foobar"));
     }
 
 }
--- build.sbt
+++ build.sbt
@@ -1,5 +1,4 @@
 name := """random-book-generator"""
-
 version := "1.0-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(PlayJava)
